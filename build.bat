311d1191ab9a58b0660545c34ea4444b69f7ecc0
MaxZhirniy <113582843+MaxZhirniy@users.noreply.github.com>
2024-11-01 14:44:28 +0700

    compile in folder out, delete res.o after compile

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 cd "i:/learns/Trash/cpp_projects/VsStart/" && I:/programs/vscodium/mingw64/bin/windres.exe res.rc -o res.o
-cd "i:/learns/Trash/cpp_projects/VsStart/" && I:/programs/vscodium/mingw64/bin/g++.exe -static VSstart.cpp I:\learns\Trash\cpp_projects\VsStart\res.o -o RunPortable.exe -static -mwindows
+cd "i:/learns/Trash/cpp_projects/VsStart/" && I:/programs/vscodium/mingw64/bin/g++.exe -static VSstart.cpp I:\learns\Trash\cpp_projects\VsStart\res.o -o out\RunPortable.exe -static -mwindows
+del "res.o" -f
